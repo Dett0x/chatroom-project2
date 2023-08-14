a15faf2d705b10a192ce6a76b59a757045ed141b
Dett0x <46972475+Dett0x@users.noreply.github.com>
2023-08-14 13:48:54 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-// Remove the import statement from this file
-const socket = io('https://chatroom-project2.vercel.app'); // Provide the correct URL
+document.addEventListener('DOMContentLoaded', () => {
+  const socket = io('https://chatroom-project2.vercel.app'); // Provide the correct URL
+  const messageInput = document.getElementById('messageInput');
+  const sendButton = document.getElementById('sendButton');
+  const messages = document.getElementById('messages');
 
-const messageInput = document.getElementById('messageInput');
-const sendButton = document.getElementById('sendButton');
-const messages = document.getElementById('messages');
+  sendButton.addEventListener('click', () => {
+    const message = messageInput.value.trim();
+    if (message !== '') {
+      socket.emit('chat message', message);
+      messageInput.value = '';
+    }
+  });
 
-sendButton.addEventListener('click', () => {
-  const message = messageInput.value.trim();
-  if (message !== '') {
-    socket.emit('chat message', message);
-    messageInput.value = '';
-  }
-});
-
-socket.on('chat message', (message) => {
-  const messageElement = document.createElement('div');
-  messageElement.textContent = message;
-  messages.appendChild(messageElement);
+  socket.on('chat message', (message) => {
+    const messageElement = document.createElement('div');
+    messageElement.textContent = message;
+    messages.appendChild(messageElement);
+  });
 });
